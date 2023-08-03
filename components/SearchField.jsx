f0aae6b35105cfda343fda82c92d340f9b17df1e
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-08-03 17:48:59 +0100

    implemented basic extraction from youtube api

---
+++ components/SearchField.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const SearchField = ({ fetchVideoData }) => {
+    const [videoUrl, setVideoUrl] = useState('');
+    const api_key = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+
+    const extractUrl = () => {
+        const videoId = videoUrl.split('v=')[1].split('&')[0];
+        console.log(videoId);
+        axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${api_key}`)
+        .then((response) => {
+            fetchVideoData(response.data.items[0].snippet);
+        })
+    }
+  return (
+    <div className="flex items-center mt-4 space-x-3 bg-[#ccc]">
+      <input
+        type="search"
+        name="videourl"
+        value={videoUrl}
+        onChange={(e) => setVideoUrl(e.target.value)}
+        placeholder="Paste YouTube url here..."
+        className="w-full text-[#000] placeholder:text-[#404040] border-none outline-none py-1 px-3 bg-transparent rounded-md"
+      />
+      <button className="text-[#000] border-none outline-none px-3 text-[14px]" onClick={extractUrl}>Extract</button>
+    </div>
+  );
+};
+
+export default SearchField;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
+import SearchField from '@/components/SearchField'
 import React from 'react'
 
-const index = () => {
+const Home = () => {
+
+  const fetchVideoData = async (videoData) => {
+    console.log(videoData)
+  }
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="w-[70vw] mx-auto mt-4">
+      <h2 className="text-center font-mono font-bold text-[24px]">Extro</h2>
+      <SearchField fetchVideoData={fetchVideoData} />
+      
     </div>
   )
 }
 
-export default index
+export default Home
